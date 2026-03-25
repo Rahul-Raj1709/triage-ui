2e630c341ba9ed3ebb3e9b10ac3915f8da5b8711
RahulRaj <rahulraj@bizframe.com>
2026-03-25 22:41:59 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TriageDashboard from "./TriageDashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground py-10">
+      <TriageDashboard />
+    </div>
+  );
+}
+
+export default App;
